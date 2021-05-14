f43a45f8eb26b124156ca877e5688c6da2d1f09f
Isabella <isabellakurnia@gmail.com>
2021-05-14 16:56:34 -0700

    Add map page

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Map from "./components/map";
+export default class App extends React.Component {
+  render() {
+    return (
+      <Map/>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 
