83cb33a807265859287e7f471fb80f582a82a0ba
kezia <kezialiman@gmail.com>
2021-05-14 20:14:06 -0700

    delete comment

--- pages/MapScreen.js
+++ pages/MapScreen.js
@@ -1,25 +1,6 @@
-/*import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-
-function MapScreen() {
-  return (
-    <MapView
-       style={{ flex: 1 }}
-       provider={PROVIDER_GOOGLE}
-       showsUserLocation
-       initialRegion={{
-       latitude: 37.78825,
-       longitude: -122.4324,
-       latitudeDelta: 0.0922,
-       longitudeDelta: 0.0421}}
-    />
-  );
-}
-export default MapScreen;*/
-
-import React, { Component } from 'react';
-import { Dimensions, StyleSheet, Text, View } from "react-native"
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import React from 'react';
+import { Dimensions, StyleSheet, View } from "react-native"
+import MapView, { Marker } from 'react-native-maps';
 
 const styles = StyleSheet.create({
 	container: {
