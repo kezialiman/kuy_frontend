f0c1f2cb55adf762a2f1963d87e7a0074f9a9c1c
kezia <kezialiman@gmail.com>
2021-05-14 20:13:08 -0700

    add marker to map

--- pages/MapScreen.js
+++ pages/MapScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 function MapScreen() {
@@ -15,4 +15,44 @@ function MapScreen() {
     />
   );
 }
+export default MapScreen;*/
+
+import React, { Component } from 'react';
+import { Dimensions, StyleSheet, Text, View } from "react-native"
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	map: {
+		width: Dimensions.get("window").width,
+		height: Dimensions.get("window").height
+	}
+})
+
+const MapScreen = (lat, lon) => {
+  return (
+    <View>
+    <MapView
+     style={styles.map}
+     initialRegion={{
+       latitude: lat,
+       longitude: lon,
+       latitudeDelta: 0.0922,
+       longitudeDelta: 0.0421
+     }}
+     provider="google"
+    >
+    <Marker
+      coordinate={{ latitude: lat,longitude: lon}}
+    />
+  </MapView>
+  </View>
+  )
+}
+
 export default MapScreen;
