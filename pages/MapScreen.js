dae52dde9c358ba1e944c0266748269829b2ec38
kezia <kezialiman@gmail.com>
2021-05-14 21:24:54 -0700

    add search bar

--- pages/MapScreen.js
+++ pages/MapScreen.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Dimensions, StyleSheet, View } from "react-native"
+import React, { useState } from 'react';
+import { Dimensions, StyleSheet, View, StatusBar, Text } from "react-native"
 import MapView, { Marker } from 'react-native-maps';
+import { SearchBar } from 'react-native-elements';
 
 const styles = StyleSheet.create({
 	container: {
@@ -16,22 +17,43 @@ const styles = StyleSheet.create({
 })
 
 const MapScreen = (lat, lon) => {
+  const [search, setSearch] = useState('');
+    
   return (
     <View>
-    <MapView
-     style={styles.map}
-     initialRegion={{
-       latitude: lat,
-       longitude: lon,
-       latitudeDelta: 0.0922,
-       longitudeDelta: 0.0421
-     }}
-     provider="google"
-    >
-    <Marker
-      coordinate={{ latitude: lat,longitude: lon}}
-    />
-  </MapView>
+      <SearchBar
+          placeholder="Type Here..."
+          onChangeText={(value) => setSearch(value)}
+          value={search}
+          round
+          containerStyle={{
+            backgroundColor: 'transparent',
+            borderTopWidth: 0,
+            borderBottomWidth: 0
+          }}
+          inputContainerStyle={{
+            backgroundColor: '#fff'
+          }}
+          cancelButtonProps={{
+            color: '#fff'
+          }}
+        />
+      <Text>TEST</Text>
+      <Text>{search}</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: lat,
+          longitude: lon,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+         }}
+        provider="google"
+      >
+      <Marker
+        coordinate={{ latitude: lat,longitude: lon}}
+      />
+      </MapView>
   </View>
   )
 }
