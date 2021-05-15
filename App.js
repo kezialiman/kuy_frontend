a2c3b2c14c9e99fd9a7f3becb36ddc21bd6fede1
Isabella <isabellakurnia@gmail.com>
2021-05-14 17:01:16 -0700

    Add init files to gitignore

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import Map from "./components/map";
-export default class App extends React.Component {
+import {
+  StackNavigator,
+} from 'react-navigation';
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Map/>
+//     )
+//   }
+// }
+
+
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen },
+});
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <Map/>
-    )
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigate('Profile')
+        }
+      />
+    );
   }
 }
-
